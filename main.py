5d2605c6efd2a43dab956f17ae89879daf6c0442
OWNER <acharyaraj71@gmail.com>
2021-02-16 18:34:47 +0545

    Added

--- main.py
+++ main.py
@@ -8,12 +8,30 @@ import time
 root = tk.Tk()
 root.withdraw()
 root.attributes('-topmost', True)
-musicFile = str(filedialog.askopenfilename())
+musicFile = list(filedialog.askopenfilenames())
 
 itter = ''
+options = [
+    ('Play', 'Play Music/Unpause Music'),
+    ('Pause', 'Pause Music'),
+    ('Stop', 'Stop Music'),
+    ('Quit / exit', 'Close Player'),
+    ('Status', 'Show Current Status of Music'),
+    ('Volume', 'Set Volume Level'),
+    ('mute/unmute', 'Mute/Unmute Audio'),
+    ('Change', 'Change Current music'),
+    ('set time', 'Set Current Time'),
+    ('Clear', 'Clear Screen'),
+    ('Next', 'Change to Next Song'),
+    ('prev', 'Change to Previous Song'),
+    ('ls', 'View List of Song'),
+    ('update repo', 'Update to GitHub'),
+    ('add', 'Add More Files')
+]
+constant = 0
 if __name__ == '__main__':
     while True:
-        data = MediaPlayer(musicFile)
+        data = MediaPlayer(str(musicFile[constant]))
         while True:
             if itter == 'play':
                 ask = 'play'
@@ -22,18 +40,51 @@ if __name__ == '__main__':
                 ask = input('user@root:~ ').lower()
             if ask == 'play':
                 if str(data.get_state()).split('.')[1] == 'Ended':
-                    musicFile = filedialog.askopenfilename()
+                    musicFile = list(filedialog.askopenfilenames())
                     itter = 'play'
                     data.stop()
                     break
                 else:
-                    data.play()
                     print('Playing Audio')
+                    data.play()
                     time.sleep(1)
             elif ask == 'pause':
                 print('Pausing Audio')
                 time.sleep(0.5)
                 data.pause()
+            elif ask == 'next':
+                constant = len(musicFile)-1 if (constant+1) >= len(musicFile) else constant+1
+                print('No More Files are there!Play Last File' if (constant+1) >= len(musicFile) else constant+1)
+                itter = 'play'
+                data.stop()
+                break
+            elif ask == 'prev':
+                constant = constant - 1
+                itter = 'play'
+                data.stop()
+                break
+            elif ask == 'choose':
+                innerAsk = input('Enter Number (ls for list): ').lower()
+                if innerAsk == 'ls':
+                    cnt = 0
+                    for item in musicFile:
+                        print('0'+str(cnt) if cnt < 10 else cnt, ') ', str(item).split('/')[-1])
+                        cnt += 1
+                    innerAsk = input('Enter Number: ')
+                else:
+                    innerAsk = int(innerAsk)
+                if innerAsk == 'exit':
+                    print('Exiting Music Player')
+                    time.sleep(1)
+                    exit()
+                else:
+                    try:
+                        constant = int(innerAsk)
+                    except Exception:
+                        pass
+                itter = 'play'
+                data.stop()
+                break
             elif ask == 'stop':
                 print('Stopping Audio')
                 time.sleep(0.5)
@@ -47,7 +98,7 @@ if __name__ == '__main__':
                 time.sleep(0.5)
                 os.system('cls')
             elif ask == 'change':
-                musicFile = filedialog.askopenfilename()
+                musicFile = filedialog.askopenfilenames()
                 itter = 'play'
                 data.stop()
                 break
@@ -77,15 +128,35 @@ if __name__ == '__main__':
                 print('Volume Level: {}'.format(data.audio_get_volume()))
             elif ask == 'volume':
                 print('Current Level: {}'.format(data.audio_get_volume()))
-                level = int(input('New Level (0 - 100): '))
-                data.audio_set_volume(level)
+                level = input('New Level (0 - 100): ')
+                if level == '':
+                    pass
+                else:
+                    data.audio_set_volume(int(level))
             elif ask == 'set time':
                 tm = int(input('Enter Time (0 - {}): '.format(data.get_length())))
                 data.pause()
                 data.set_time(tm)
                 data.play()
+            elif ask == 'options' or ask == 'opt':
+                print('Options',' '*(15-len('Options')), ':','Functions')
+                print('\n')
+                for item in options:
+                    print(item[0],' '*(15-len(item[0])),':',item[1])
             elif ask == 'test':
                 print(data.get_time())
                 data.set_time(3400)
+            elif ask == 'update repo':
+                os.system('git pull&&git push')
+            elif ask == 'add':
+                getItem = list(filedialog.askopenfilenames())
+                musicFile.append(getItem)
+                time.sleep(0.2)
+                print('{} Files added to Current List'.format(len(getItem)))
+            elif ask == 'ls':
+                cnt = 0
+                for item in musicFile:
+                    print('0'+str(cnt) if cnt < 10 else cnt, ') ', str(item).split('/')[-1])
+                    cnt += 1
             else:
                 print(data.is_playing())
